66779035c16bf8ed9357d4f44ff20bf1a087ca3b
Taisia <meg826733@gmail.com>
2022-09-03 18:01:22 +0500

    first commit

---
+++ src/test/java/guru/qa/TestSelenidePage.java
@@ -0,0 +1,26 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestSelenidePage {
+
+    @Test
+    void shouldFindSoftAssertionsPage() {
+
+        //открываем страницу
+        open("https://github.com/");
+        // вводим в поле поиска selenide и нажимаем на enter
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        // ищем Wiki и переходим в нее
+        $("[data-search-type=Wikis]").click();
+        // проверяем, что в результатах поиска есть статья SoftAssertions
+        $("#wiki_search_results").shouldHave(text("SoftAssertions"));
+        //переходим в статью
+        $("[title=SoftAssertions]").click();
+        //ищем в статье пример кода на JUnit5
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
